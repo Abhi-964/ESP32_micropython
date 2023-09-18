e2abab3b5ba263e3f57d5345d7fbe81a3bbd4be1
Abhi-964 <144892816+Abhi-964@users.noreply.github.com>
2023-09-18 18:07:49 +0530

    ESP32 neopixel led and its code

---
+++ hello world.py
@@ -0,0 +1,1 @@
+print("hello to the world of ESP32S3 and micropython")
---
+++ neopixal/main.py
@@ -0,0 +1,26 @@
+#micropython
+
+#controlling the neopixel led
+
+import machine
+import neopixel
+import time
+
+#object for neopixel LED
+
+np = neopixel.NeoPixel(machine.Pin(48),1)
+
+while True:
+    try:
+        np[0] = [255,0,0]
+        np.write()
+        time.sleep(2)
+        '''np[0] = [0,255,0]
+        np.write()
+        time.sleep(2)
+        np[0] = [0,0,255]
+        np.write()
+        time.sleep(2)'''
+    except KeyboardInterrupt:
+        print("Exit")
+        break
